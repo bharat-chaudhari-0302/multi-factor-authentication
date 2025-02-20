e8767a48a8395b4c4e1b9beb77e9bdb9f923231a
Bharat Chaudhari <bharat.chaudhari.0302@gmail.com>
2025-02-20 16:20:11 +0530

    initial commit

---
+++ src/main/java/com/multi/factor/authentication/poc/MultiFactorAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.multi.factor.authentication.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFactorAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFactorAuthenticationApplication.class, args);
+	}
+
+}
